4601315e864d2c1c3c7f5d582524a118f577926b
yuzaijiang <1015118515@qq.com>
2020-10-11 23:59:34 +0800

    hello yuzaijiang2

--- src/demo1.java
+++ src/demo1.java
@@ -2,7 +2,7 @@ public class demo1 {
     public static void main(String[] args) {
         // write your code here
         System.out.println("shanghai is a good city");
-
+        System.out.println("beijing is a good city");
     }
 
 }
