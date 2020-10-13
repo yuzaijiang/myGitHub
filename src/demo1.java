fe36226162f72216d278ac376c762e3bd808bd45
yuzaijiang <1015118515@qq.com>
2020-10-13 08:16:14 +0800

    喻在江13号提交

--- src/demo1.java
+++ src/demo1.java
@@ -3,6 +3,8 @@ public class demo1 {
         // write your code here
         System.out.println("shanghai is a good city");
         System.out.println("beijing is a good city");
+        System.out.println("2020-10-13");
+        System.out.println("2020-10-13-02");
     }
 
 }
